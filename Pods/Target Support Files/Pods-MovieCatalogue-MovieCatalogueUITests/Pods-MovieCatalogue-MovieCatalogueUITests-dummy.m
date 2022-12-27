77164218a80bf048475a18024ded511b6d581fb2
darshanpanchalneosoft <113101368+darshanpanchalneosoft@users.noreply.github.com>
2022-12-27 18:47:42 +0530

    pod configuration

---
+++ Pods/Target Support Files/Pods-MovieCatalogue-MovieCatalogueUITests/Pods-MovieCatalogue-MovieCatalogueUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieCatalogue_MovieCatalogueUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MovieCatalogue_MovieCatalogueUITests
+@end
---
+++ Pods/Target Support Files/Pods-MovieCatalogueTests/Pods-MovieCatalogueTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieCatalogueTests : NSObject
+@end
+@implementation PodsDummy_Pods_MovieCatalogueTests
+@end
